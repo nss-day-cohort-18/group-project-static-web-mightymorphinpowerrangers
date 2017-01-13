e7906623b2e7f5d6244dde0940bb25457cf5f1bf
Zak Spence <zakspence@gmail.com>
2017-01-13 16:10:34 -0600

    Began placing product information into objects. I have four, James has the other four.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var productDetails = [];
+
+var productObj = {
+	
+	petalCastle: {name: "Petal Castle", description: , price: , img: }
+	tunnelOfLove: {name: "Love Tunnel", description: , price: , img: }
+	mapleHill: {name: "Maple Hill", description: , price: , img: }
+	hauntedHouse: {name: "THE VILLISCA AXE MURDER HOUSE", description: "On a quiet residential street in this small town sits an old white frame house. On a dark evening, the absence of lights and sounds are the first indication to visitors that this house is different from the other homes that surround it. Upon closer inspection, you'll notice her doors and windows are tightly closed and covered. An outhouse in the backyard suggests that this house does not occupy a place in the 21st century but somehow belongs in another era or another story. A weather-beaten sign warns rather than welcomes. This is the 'Murder House'.", price: "10000000" , img: function(){ var img = new Image(); img.src = 'images/product-data/haunted/haunted-house.jpg'}}
+
+}
